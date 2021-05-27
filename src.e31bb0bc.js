1d6c5220b73876d3b5e07dbac44c0d4004d1d12d
preranaarage0612 <preranaarage612@rediffmail.com>
2021-05-27 21:07:05 +0530

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -50348,6 +50348,18 @@ const CONTRACT_NAME = "dev-1622112474049-6887379" || 'voting';
 
 function getConfig(env) {
   switch (env) {
+    case 'production':
+    case 'mainnet':
+      return {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        contractName: CONTRACT_NAME,
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org',
+        explorerUrl: 'https://explorer.testnet.near.org'
+      };
+
+    case 'development':
     case 'testnet':
       return {
         networkId: 'testnet',
